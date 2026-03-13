9de8acfdecdbdfef6ed095df4c47cdefa3361ab6
JayzirTech <jayzirslaider18@gmail.com>
2026-03-13 15:15:28 -0500

    Code corrected. No errors

--- main.py
+++ main.py
@@ -1,121 +1,109 @@
-#from... import
-#from total_sales import calculate_total_sale
-#from total_sales import calculate_total_sales_day
+from numerical_validation import numericalValidationFloat
+from numerical_validation import numericalValidationInt
 
-#Variables
-programStatus = True
-sistemStatus = True
-saleStatus = True
-addProducts = True
-newSales = True
+# Variables
 sales=[]
 salesDay=[]
+saleStatus=True
+addProducts=True
+sistemStatus=True
+addNewSales=True
+programStatus=True
 totalSales=0
-totalDay=0
+salesDayTotal=0
 
+# Program
+print()
+print("Welcome to the inventory management system")  #Welcome message
 print("--------------------------------")
-print("Daily Sales Record")
 
-while programStatus:    #Starting the program
-    while sistemStatus:   #Starting the system
-        while saleStatus:   #Starting the sale
-            print("")
-            name=input("Please, Enter the product name: ")  #Input for product name
-            price=input("Please, Enter the product value: ") #Input for product value
-            quantity=input("Please, Enter the quantity of the product: ") #Input for product quantity
+while programStatus:   #Loop to ask the user if they want to start the program
+    while sistemStatus:   #Loop to ask the user if they want to start the program
+        while saleStatus:   #Loop to add products to the sale
+            name=input("Please, Enter the product name: ")  #Enter the product name
+            price=numericalValidationFloat()  #Call the function to validate the price input
+            quantity=numericalValidationInt()  #Call the function to validate the quantity input
+            print("product added successfully!")
 
-            sale=[name, price, quantity]   #Creating a list with the product details (this part is not implemented yet, the sale list is not being used)
-            sales.append(sale)    #Adding the sale to the sales list (this part is not implemented yet, the sales list is not being used)
-            print("Product added successfully!")   #Print a message confirming that the product was added successfully
+            sale=[name, price, quantity]    #Create a list with the product name, price and quantity
+            sales.append(sale)  #Add the product to the sales list
+            salesDay.append(sale)   #Add the product to the salesDay list to keep track of the sales of the day
+            print()
 
-            print("--------------------------------")
-            addProduct=input("Want add more product? (Yes) or (No): ").lower() #Input for adding more products
-
-            if addProduct == "yes": #If the user wants to add more products, the saleStatus will be True and the program will ask for the product details again
-                saleStatus=True
-
-            elif addProduct == "no":    #If the user doesn't want to add more products, the saleStatus will be False and the program will ask if the user wants to register a new sale
-                saleStatus=False
-                salesDay=sales   #Create a list with the sales of the day (this part is not implemented yet, the sales list is not being used)
-            else:
-                print("Invalid option, try again")  #If the user enters an invalid option, the program will print an error message and ask for the input again
-
-                while addProducts:  #If the user enters an invalid option, the program will enter this loop until the user enters a valid option
-                    addProduct=input("Want add more product? (Yes) or (No): ").lower()
-                    if addProduct == "yes":
-                        saleStatus=True
-                        addProducts=False
-                    elif addProduct == "no":
-                        saleStatus=False
-                        addProducts=False
-                        salesDay=sales   #Create a list with the sales of the day (this part is not implemented yet, the sales list is not being used)
-                    else:
-                        print("--------------------------------")
-                        print("Invalid option, try again")
-
+            while addProducts:  
+                addProduct=input("Want add more product? (Yes) or (No): ").lower()  #Ask the user if they want to add more products to the sale
+                if addProduct == "yes":
+                    addProducts=False
+                elif addProduct == "no":
+                    saleStatus=False
+                    addProducts=False
+                else:
+                    print("--------------------------------")
+                    print("Invalid option, try again")  #If the user enters an invalid option, it will ask again
             addProducts=True
 
+        print()
         print("--------------------------------")
+
+        #Print the products added to the sale and calculate the total sale
         for sale in sales:  
-            number = sales.index(sale) + 1   #Get the index of the sale in the sales list and add 1 to it (this part is not implemented yet, the sales list is not being used)  
-            print(f"Product {number}: {sale[0]}, Value: {sale[1]}, Quantity: {sale[2]}, Total: {float(sale[1]) * int(sale[2])}")   #Print the product details (this part is not implemented yet, the sales list is not being used)
-            subTotalSale=float(sale[1]) * int(sale[2])   #Calculate the total sale for each product (this part is not implemented yet, the sales list is not being used)
-            totalSales= totalSales + subTotalSale   #Calculate the total sales for the day by adding the total sale for each product (this part is not implemented yet, the sales list is not being used)
+            number = sales.index(sale) + 1
+            subTotalSale=sale[1] * sale[2]
+            print(f"Product {number}: {sale[0]} | Value: ${sale[1]} | Quantity: {sale[2]} | Total: ${subTotalSale}")   #
+            totalSales= totalSales + subTotalSale
             
-        print(f"Total sale is: {totalSales}")   #Print the total sale for each product (this part is not implemented yet, the sales list is not being used)
-    
-        while newSales: 
-            print("--------------------------------")
-            newSale=input("Want to register a new sale? (Yes) or (No): ").lower()   #Ask if the user wants to register a new sale
-            if newSale == "yes":    #If the user wants to register a new sale, the saleStatus will be True and the program will ask for the product details again
+        #Print the total sale    
+        print(f"Total sale is: ${totalSales}")
+        print("Thank for your purchase!")
+        print("--------------------------------")
+        print()
+        sales.clear()  #Clear the sales list for the next sale
+        totalSales=0   #Reset the total sales for the next sale
+
+        while addNewSales:   #Loop to ask the user if they want to add a new sale
+            addNewSale=input("Want add a new sale? (Yes) or (No): ").lower()  #Ask the user if they want to add a new sale
+            if addNewSale == "yes":
                 saleStatus=True
-                newSales=False
-            elif newSale == "no":   #If the user doesn't want to register a new sale, the saleStatus will be False and the sistemStatus will be False, ending the program
-                saleStatus=False
+                addNewSales=False
+            elif addNewSale == "no":
                 sistemStatus=False
-                newSales=False
-                sales.clear()   #Clear the sales list for the next sale (this part is not implemented yet, the sales list is not being used)
+                addNewSales=False
             else:
-                print("Invalid option, try again")  #If the user enters an invalid option, the program will print an error message and ask for the input again
-                saleStatus=False
+                print("--------------------------------")
+                print("Invalid option, try again")  #If the user enters an invalid option, it will ask again
+        addNewSales=True
 
-        newSales=True
-
-    #menu
-    print("--------------------------------")
-    print("Daily Sales Record")
-    print("1. Register a new sale")
-    print("2. View sales records")
+    print("Main menu")
+    print("1. Add a new sale")
+    print("2. View sales of the day")
     print("3. Exit")
+    Option=input("Enter the option number: ")   #Ask the user to choose an option
 
-    option=input("Please, select an option: ")    #Input for menu option
-
-    if option == "1":   #If the user selects option 1, the program will start the sale process again
+    if Option == "1":
+        saleStatus=True
+        addProducts=True
         sistemStatus=True
         saleStatus=True
-    elif option == "2": #If the user selects option 2, the program will print the sales records (this part is not implemented yet)
+        print()
+    elif Option == "2":
+        print()
+        print("Sales of the day")
         print("--------------------------------")
-        print("Sales records")
-        number=len(salesDay)   
-        print(f"holaaaaa {number}") 
-        for sale in salesDay:  
-            number = salesDay.index() + 1   #Get the index of the sale in the salesDay list and add 1 to it (this part is not implemented yet, the salesDay list is not being used)  
-            print(f"Product {number}: {sale[0]}, Value: {sale[1]}, Quantity: {sale[2]}, Total: {float(sale[1]) * int(sale[2])}")   #Print the product details (this part is not implemented yet, the salesDay list is not being used)
-            subTotalSalesDay= float(sale[1]) * int(sale[2])   #Calculate the total sale for each product (this part is not implemented yet, the salesDay list is not being used)
-            totalDay= totalDay + subTotalSalesDay   #Calculate the total sales for the day by adding the total sale for each product (this part is not implemented yet, the salesDay list is not being used)
-        print(f"Total sales for the day: {totalDay}")   #Print the total sales for the day (this part is not implemented yet, the totalDay variable is not being used)
-    elif option == "3": #If the user selects option 3, the program will print a goodbye message and end the program
+        for sale in salesDay:  #Print the products added to the salesDay list and calculate the total sales of the day
+            number = salesDay.index(sale) + 1
+            subTotalSale=sale[1] * sale[2]
+            print(f"Product {number}: {sale[0]} | Quantity: {sale[2]}")   #
+            salesDayTotal=subTotalSale + salesDayTotal
+        print("Total sales of the day: $", salesDayTotal)   #Print the total sales of the day
         print("--------------------------------")
-        print("Thank you for using the Daily Sales Record program. Goodbye!")
-        programStatus=False
-    else:
-        print("Invalid option, try again")  #If the user enters an invalid option, the program will print an error message and ask for the input again
+        print()
+        salesDayTotal=0   #Reset the total sales of the day for the next time the user wants to view the sales of the day
 
-"""Mira, ten en cuenta que cuando realizas un nueva compra, a persar de terminar la anterior, 
-el programa no borra los datos de la compra anterior, por lo que al momento de registrar una nueva compra, 
-el programa sigue sumando los datos de la compra anterior, por lo que el total de la nueva compra 
-es el total de la compra anterior más el total de la nueva compra, lo mismo pasa con las ventas del día, 
-por lo que al momento de registrar una nueva venta, el programa sigue sumando los datos de la venta anterior, 
-por lo que el total de las ventas del día es el total de las ventas del día anterior más el total de las 
-ventas del día actual. Para solucionar esto, puedes agregar una función para limpiar los datos de la compra anterior 
-y las ventas del día anterior al momento de registrar una nueva compra o una nueva venta."""
+    elif Option == "3":
+        programStatus=False
+        print()
+        print("Thank you for using the inventory management system, see you next time!")  #Exit message
+        print("")
+    else:
+        print("--------------------------------")
+        print("Invalid option, try again")  #If the user enters an invalid option, it will ask again
